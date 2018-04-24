c2adfe75c4b2cfb0aac9331e17a7951533586eb9
Kyle Morton <kylemortondevelopment@gmail.com>
2018-04-24 13:24:57 -0500

    checking in db config changes (moving to Azure)

--- AngularForm.Data/FormModel.Designer.cs
+++ AngularForm.Data/FormModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\kylem\source\repos\AngularFormApp\AngularForm.Data\FormEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\MORTONM\source\repos\AngularFormApp\AngularForm.Data\FormModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
